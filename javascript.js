4909d5b0c599aaf243fda3dd450b18e5426ab289
patriciaho <patricia.p.ho@gmail.com>
2014-01-09 18:53:39 -0800

    Add draw logic

--- javascript.js
+++ javascript.js
@@ -3,18 +3,18 @@ var turnCount = 0
 var board = ['','','','','','','','',''];
 var xWins = 0;
 var oWins = 0;
+var gameover = false;
 
 function setupPage() {
 var cells = document.getElementsByClassName('box');
 for(var i in cells) {
-	cells[i].onclick = function(){
-		myClick(this);
-	};
+	cells[i].onclick = myClick;
 }
 }
 
-function myClick(cell) {
-    if (cell.innerHTML == '') {
+function myClick() {
+	cell = this;
+    if (cell.innerHTML == '' && gameover == false) {
     	if (turn == "X") {
         turn = "O";
     	} else if (turn == "O") {
@@ -22,8 +22,8 @@ function myClick(cell) {
     	}
     	cell.innerHTML= turn;
     	board[cell.id.charCodeAt(0)-97] = turn;
-    	var classes = cell.className;
-	 	cell.setAttribute('class', classes + ' taken');
+   //  	var classes = cell.className;
+	 	// cell.setAttribute('class', classes + ' taken');
 	 	if ( "X,X,X" == [board[0], board[1], board[2]].join() ||
 		"X,X,X" == [board[3], board[4], board[5]].join() ||
 		"X,X,X" == [board[6], board[7], board[8]].join() ||
@@ -35,7 +35,7 @@ function myClick(cell) {
 			banner.innerHTML = ('X wins');
 			banner.setAttribute('style','background: #DF38B1');
 			banner.setAttribute('class','animated fadeIn');
-			turn = "";
+			gameover = true;
 		// xWins += 1;
 		// turnCount += 1;
 		// document.getElementById('wintrackerx').innerHTML = "X = " + xWins;
@@ -51,10 +51,18 @@ function myClick(cell) {
 			banner.innerHTML = ('O wins');
 			banner.setAttribute('style','background: #DF38B1');
 			banner.setAttribute('class','animated fadeIn');
-			turn = "";
+			gameover = true;
 		// oWins += 1;
 		// turnCount += 1;
 		// document.getElementById('wintrackero').innerHTML = "O = " + xWins;
+		}
+		if (board[0] != '' && board[1] != '' && board[2] != '' && 
+			board[3] != '' && board[4] != '' && board[5] != '' && 
+			board[6] != '' && board[0] != '' && board[0] != '' ) {
+			banner.innerHTML = ('Draw');
+			banner.setAttribute('style','background: #DF38B1');
+			banner.setAttribute('class','animated fadeIn');
+			gameover = true;
 		}
 	} 
 }
@@ -71,5 +79,9 @@ function myClick(cell) {
 // 	g.innerHTML = "";
 // 	h.innerHTML = "";
 // 	i.innerHTML = "";
+// 	// var cells = document.getElementsByClassName('box');
+// 	// for(var x in cells) {
+// 	// cells[i] = console.log(cells)
+// 	// }	
 // }
 
