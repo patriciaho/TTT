e2b3d967376a17a704d83264efff0d3de63b9e2d
patriciaho <patricia.p.ho@gmail.com>
2014-01-10 11:50:37 -0800

    Refactor reset()

--- javascript.js
+++ javascript.js
@@ -1,33 +1,31 @@
+var gameover = false;
 var turn = "O";
-var turnCount = 0
+var turnCount = 0;
 var board = ['','','','','','','','',''];
 var xWins = 0;
 var oWins = 0;
-var gameover = false;
-
+var draw = 0;
 
 function setupPage() {
-var cells = document.getElementsByClassName('box');
-for(var i in cells) {
-	cells[i].onclick = myClick;
-}
-// winstatus.innerHTML = turn + "turn";
+	var cells = document.getElementsByClassName('box');
+	for(var i in cells) {
+		cells[i].onclick = myClick;
+	}
 }
 
 function myClick() {
 	body.setAttribute('class', '');
-	cell = this;
-	winstatus.innerHTML = turn + "turn"
+	var cell = this;
+	winstatus.innerHTML = turn + " turn"
     if (cell.innerHTML == '' && gameover == false) {
     	if (turn == "X") {
-        turn = "O";
-    	} else if (turn == "O") {
-        turn = "X";
+        	turn = "O";
+    	} 
+    	else if (turn == "O") {
+        	turn = "X";
     	}
     	cell.innerHTML= turn;
     	board[cell.id.charCodeAt(0)-97] = turn;
-   //  	var classes = cell.className;
-	 	// cell.setAttribute('class', classes + ' taken');
 	 	if ( "X,X,X" == [board[0], board[1], board[2]].join() ||
 		"X,X,X" == [board[3], board[4], board[5]].join() ||
 		"X,X,X" == [board[6], board[7], board[8]].join() ||
@@ -37,12 +35,10 @@ function myClick() {
 		"X,X,X" == [board[0], board[4], board[8]].join() ||
 		"X,X,X" == [board[6], board[4], board[2]].join() ) {
 			banner.innerHTML = ('X wins');
-			banner.setAttribute('style','background: #DF38B1');
-			banner.setAttribute('class','animated fadeIn');
+			endBanner();
 			gameover = true;
 			xWins += 1;
-		// turnCount += 1;
-		document.getElementById('wintrackerx').innerHTML = "X = " + xWins;
+			document.getElementById('wintrackerx').innerHTML = "X Wins= " + xWins;
 		}
 		if ( "O,O,O" == [board[0], board[1], board[2]].join() ||
 		"O,O,O" == [board[3], board[4], board[5]].join() ||
@@ -53,58 +49,40 @@ function myClick() {
 		"O,O,O" == [board[0], board[4], board[8]].join() ||
 		"O,O,O" == [board[6], board[4], board[2]].join() ) {
 			banner.innerHTML = ('O wins');
-			banner.setAttribute('style','background: #DF38B1');
-			banner.setAttribute('class','animated fadeIn');
+			endBanner();
 			gameover = true;
 			oWins += 1;
-		// turnCount += 1;
-		document.getElementById('wintrackero').innerHTML ="O = " + oWins;
+			document.getElementById('wintrackero').innerHTML ="O Wins= " + oWins;
 		}
 		if (gameover == false && board[0] != '' && board[1] != '' && board[2] != '' && 
 			board[3] != '' && board[4] != '' && board[5] != '' && 
 			board[6] != '' && board[7] != '' && board[8] != '' ) {
 			banner.innerHTML = ('Draw');
-			banner.setAttribute('style','background: #DF38B1');
-			banner.setAttribute('class','animated fadeIn');
+			endBanner();
 			gameover = true;
+			draw += 1;
+			document.getElementById('drawtracker').innerHTML ="Draw = " + oWins;
 		}
 	} 
 }
 
+function endBanner() {
+	banner.setAttribute('style','background: #DF38B1');
+	banner.setAttribute('class','animated fadeIn');
+}
+
 function reset() {
-	banner.innerHTML = "Tic Tac Toe";
-	banner.setAttribute('class', '');
-	body.setAttribute('class', 'animated fadeIn');
-	banner.setAttribute('style','background: rgba(100, 100, 100, 1)');
-	// for each x in (a)
-	a.innerHTML = "";
-	b.innerHTML = "";
-	c.innerHTML = "";
-	d.innerHTML = "";
-	e.innerHTML = "";
-	f.innerHTML = "";
-	g.innerHTML = "";
-	h.innerHTML = "";
-	i.innerHTML = "";
 	gameover = false;
-	board[0] = "";
-	board[1] = "";
-	board[2] = "";
-	board[3] = "";
-	board[4] = "";
-	board[5] = "";
-	board[6] = "";
-	board[7] = "";
-	board[8] = "";
-	// turn = "O";
-	
-	// banner.setAttribute('class','');
-	// banner.setAttribute('class', '');
-	// body.setAttribute('class', '');
-	// body.setAttribute('class','animated fadeIn');
-	// var cells = document.getElementsByClassName('box');
-	// for(var x in cells) {
-	// cells[i] = console.log(cells)
-	// }	
+	body.setAttribute('class', 'animated fadeIn');
+	banner.innerHTML = "Tic Tac Toe";
+	banner.setAttribute('style','background: rgba(100, 100, 100, 1)');
+	banner.setAttribute('class', '');
+	var cells = document.getElementsByClassName('box');
+	for(var i in cells) {
+		cells[i].innerHTML = "";
+	}
+	for(var n=0; n < cells.length; n++) {
+		board[n] = "";
+	}	
 }
 
